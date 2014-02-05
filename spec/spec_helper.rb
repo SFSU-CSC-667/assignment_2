e9ee39dc5cb1194ca2b7c88696fbe0bc25c4f531
John Roberts <roberts.john@gmail.com>
2014-02-04 22:15:52 -0800

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ $: << '../'
 Dir.glob('*.rb').each {|f| puts "requiring #{f}"; require f}
 
 RSpec.configure do |config|
-	config.expect_with :rspec do |c|
-		c.syntax = :expect
-	end
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
